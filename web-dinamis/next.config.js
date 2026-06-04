692b66355e51af70e440ef07e29a15c9951efa68
akhariss <its.kenzy123@gmail.com>
2026-06-04 19:02:45 +0700

    config

--- web-dinamis/next.config.js
+++ web-dinamis/next.config.js
@@ -1,12 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.bing.com',
+        pathname: '/images/*',
+      },
     ],
+  },
+  webpack(config) {
+    config.resolve ??= {};
+    config.resolve.alias ??= {};
+    config.resolve.alias['node:buffer'] = 'buffer/';
+    return config;
   },
 };
 
